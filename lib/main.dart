792d31982d99faf739c9fe4644c3e5c0fb966d03
ArshadBava <arshadbavam@gmail.com>
2022-04-28 10:34:48 +0530

    Changed background color

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
-      backgroundColor: Colors.brown,
+      backgroundColor: Colors.purple,
       appBar: AppBar(
         title: Center(child: Text("I AM POOR")),
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.green,
       ),
      body: Center(
        child:Image(
